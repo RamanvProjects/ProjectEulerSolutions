7b47e78816afc38bb74c3e094db92669d51f4371
ramanv <ramanv@D-128-208-138-148.dhcp4.washington.edu>
2014-12-05 02:25:11 -0800

    Added Java folder and solution

---
+++ Java/firstSolution.java
@@ -0,0 +1,15 @@
+public class firstSolution {
+	public static void main(String[] args) {
+		int sum = 0;
+		int max = 1000; //change this value for higher sums
+		for (int i = 1; i < max; i++) {
+			if (i % 3 == 0) {
+				sum += i;
+			} else if (i % 5 == 0) {
+				sum += i;
+			}	
+		}
+
+		System.out.println(sum);
+	}
+}
